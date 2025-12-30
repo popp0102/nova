5101097817f8b9955bc2950e9924eb4e623b63fd
Jason Poppler <popp0102@gmail.com>
2025-12-30 15:50:53 -0700

    Initial commit to nova

---
+++ lib/components/Nova.tsx
@@ -0,0 +1,8 @@
+export default function Nova() {
+  return (
+    <>
+      <h1>New Package Created</h1>
+      <h2>Nova ready to go!</h2>
+    </>
+  )
+}
---
+++ lib/index.ts
@@ -0,0 +1,3 @@
+import Nova from './components/Nova';
+
+export default Nova;
