7e1f108dc1ec088b94d91ef6a1907fb348a505ce
Jason Poppler <popp0102@gmail.com>
2026-01-01 16:48:26 -0700

    update badge component to have left and right icons options

--- observatory/stories/Badge.stories.js
+++ observatory/stories/Badge.stories.js
@@ -1,4 +1,5 @@
 import { View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 import Badge from '../../lib/components/Badge';
 
 export default {
@@ -8,8 +9,20 @@ export default {
 
 export const Default = {
   render: () => (
-    <View style={{ flex: 1, padding: 16, alignItems: 'center', justifyContent: 'center' }}>
-      <Badge>Solved</Badge>
+    <View style={{ flex: 1, padding: 16, alignItems: 'center', justifyContent: 'center', gap: 16 }}>
+      <Badge leftIcon={<Ionicons name="checkmark-circle" size={20} color="white" />}>
+        Left Icon
+      </Badge>
+      <Badge rightIcon={<Ionicons name="arrow-forward" size={20} color="white" />}>
+        Right Icon
+      </Badge>
+      <Badge
+        leftIcon={<Ionicons name="star" size={20} color="white" />}
+        rightIcon={<Ionicons name="close" size={20} color="white" />}
+      >
+        Both Icons
+      </Badge>
+      <Badge>No Icons</Badge>
     </View>
   ),
 };
